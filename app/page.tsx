b63bb2a1ee7db2f2c4998aab93b4068690eed96a
lucianosants <lucianola18@gmail.com>
2023-05-25 17:19:15 -0300

    chore: initial setup frontend

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <main className="flex h-screen flex-1 items-center justify-center bg-zinc-900 text-purple-400">
+                Building...
+            </main>
+        </>
+    );
+}
